d9ba644a6da23e3704f2f276fa29989b3fa033fe
pratishthasingh1 <68629518+pratishthasingh1@users.noreply.github.com>
2020-12-13 12:45:16 -0600

    Add files via upload

--- charts.js
+++ charts.js
@@ -3,8 +3,7 @@ function init() {
     var selector = d3.select("#selDataset");
   
     // Use the list of sample names to populate the select options
-    d3.json("https://raw.githubusercontent.com/pratishthasingh1/bacteria_analysis/main/samples.json").then((metaData) => {
-      var sampleNames = metaData.names;
+      d3.json("https://raw.githubusercontent.com/pratishthasingh1/bacteria_analysis/main/samples.json").then((data) => {var sampleNames = data.names;
       sampleNames.forEach((sample) => {
         selector
           .append("option")
@@ -14,7 +13,7 @@ function init() {
 
       // Use the first sample from the list to build the initial plots
       var firstSample = sampleNames[0];
-
+      buildCharts(firstSample);
       buildMetadata(firstSample);
     });
   }
@@ -25,13 +24,14 @@ function init() {
   function optionChanged(newSample) {
     // Fetch new data each time a new sample is selected
     buildMetadata(newSample);  
+    buildCharts(newSample);
 }
 
   
   // Demographics Panel
 function buildMetadata(sample) {
-  d3.json("https://raw.githubusercontent.com/pratishthasingh1/bacteria_analysis/main/samples.json").then((data) => {
-    var metadata = data.metadata;
+  d3.json("https://raw.githubusercontent.com/pratishthasingh1/bacteria_analysis/main/samples.json").then((metadata) => {
+    var metadata = metadata.metadata;
       // Filter the data for the object with the desired sample number
     var resultArray = metadata.filter(sampleObj => sampleObj.id == sample);
     var result = resultArray[0];
@@ -49,24 +49,25 @@ function buildMetadata(sample) {
       PANEL.append("h6").text(`${key.toUpperCase()}: ${value}`);
     });
   
-    });
-  }
+  });
+}
   
   // Create the buildChart function.
   function buildCharts(sample) {
     // Use d3.json to load the samples.json file
-    d3.json("https://raw.githubusercontent.com/pratishthasingh1/bacteria_analysis/main/samples.json").then((data) => {
-      console.log(data);
+    d3.json("https://raw.githubusercontent.com/pratishthasingh1/bacteria_analysis/main/samples.json").then((metadata) => {
+      console.log(metadata);
   
       // Create a variable that holds the samples array.
-      var samplesArray = data.samples;
+      var samplesArray = metadata.samples;
       // Create a variable that filters the samples for the object with the desired sample number.
-      var resultArray = samplesArray.filter(f => f.id == sample);
+      var resultArray = samplesArray.filter(sampleObj => sampleObj.id == sample);
 
       // 1. Create a variable that filters the metadata array for the object with the desired sample number.
       //var selectedSample = resultArray.filter(f => f.id == sample);
       // Create a variable that holds the first sample in the array.
       var firstSample = resultArray[0]; 
+      var firstMeta = samplesArray[0];
   
       // Create variables that hold the otu_ids, otu_labels, and sample_values.
       var otu_ids = firstSample.otu_ids;
@@ -76,18 +77,19 @@ function buildMetadata(sample) {
     //console.log(otu_labels)
     //console.log(sample_values)
 
+
     // Create the yticks for the bar chart.
     // Hint: Get the the top 10 otu_ids and map them in descending order
     // so the otu_ids with the most bacteria are last.
       // var top_ten = otu_ids.slice(0,10);
       // var top_ten_samples = samples_values.slice(0.10);
-      var xticks = samples_values.slice(0,10).reverse();
+      var xticks = samples_values
       var yticks = otu_ids.slice(0,10).map(d => `otu${d}`).reverse();
       var hover_text = otu_labels.slice(0,10).reverse();
      
   
       // Create the trace for the bar chart.
-      var barData = [{
+  var barData = [{
         x: xticks,
         y: yticks,
         text: hover_text,
@@ -97,7 +99,7 @@ function buildMetadata(sample) {
     
     
 // Create the layout for the bar chart.
-    var barLayout = {
+  var barLayout = {
       title: "Top 10 Bacteria Cultures Found",
       margin:{ t: 25, l:150}
     };
@@ -112,9 +114,9 @@ function buildMetadata(sample) {
     mode: "markers",
     type: "scatter",
     marker: {
-        size: sample_values,
+        size: samples_values,
         color: otu_ids,
-        colorscale: "YlGnBu"    
+        colorscale: "Earth"    
     }
   }];
 
@@ -132,49 +134,32 @@ function buildMetadata(sample) {
   Plotly.newPlot("bubble", bubbleData, bubbleLayout);
 
 
-// Create a variable that filters the metadata array for the object with the desired sample number.
-  var metadata_SelId = data.metadata.filter(data => data.id == sample);
-
 // create a variable that holds the washing frequency.
-    var washFreq = metadata_SelId[0].wfreq;
-
+  var washFreq = parseFloat(firstMeta.wfreq);
 
   // 4. Create the trace for the gauge chart.
-  var gaugeData = [{
-    domain: { x: [0, 1], y: [0, 1] },
+  var gaugeData = {
+    domain: { x: [0, 1]},
     value: washFreq,
-    bar: {color: 'black'},
-    bgcolor: "white",
+    bgcolor: "black",
+    title: { text: "Belly Button Washing Frequency" },
+    type: "indicator",
+    mode: "gauge+number",
     gauge: {
-      axis: {range : [null,10],tickwidth:1},
+      axis: {range : [null,10], tickcolor: 'black'},
+      bar: {color: 'black'},
+      bgcolor:'white',
+      borderwidth: 1,
       steps: [
         { range: [0, 2], color: "red" },
         { range: [2, 4], color: "orange" },
         { range: [4, 6], color: "yellow" },
         { range: [6, 8], color: "lightgreen" },
-        { range: [8, 10], color: "green" }]
-    },
-    title: { text: "Belly Button Washing Frequency" },
-    type: "indicator",
-    mode: "gauge+number"
-    }
-  ];
+        { range: [8, 10], color: "green" },
+      ]}};
 
   // 5. Create the layout for the gauge chart.
-  var gaugeLayout = {
-    autosize: true,
-    annotations: [{
-      xref: 'paper',
-      yref: 'paper',
-      x: 0.5,
-      xanchor: 'center',
-      y: 0,
-      yanchor: 'center',
-      text: "The gauge displays your belly button weekly washing frequency",
-      showarrow: false
-    }]
-  };
-
+  var gaugeLayout = { width: 600, height: 500};
 
   // 6. Use Plotly to plot the gauge data and layout.
   Plotly.newPlot("gauge", gaugeData, gaugeLayout);
